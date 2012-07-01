9f6d92e84b0a6a63e769ba49285262bc29070f65
stephenwil <stephenwil@gmail.com>
2012-07-01 10:35:17 +0100

    update to variables, modify less files to suit my grid

--- compile.bat
+++ compile.bat
@@ -1,4 +1,3 @@
 recess --compile less/bootstrap.less > bootstrap.css
 recess --compile less/responsive.less > bootstrap-responsive.css
-copy /y bootstrap.css "..\..\Days to Remember\website\technical\bootstrap\css\"
-copy /y bootstrap-responsive.css "..\..\Days to Remember\website\technical\bootstrap\css\"
+xcopy /y *.css "..\..\Days to Remember\website\technical\bootstrap\css\"
